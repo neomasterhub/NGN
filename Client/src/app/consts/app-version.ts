6fd1140e260bba470bb04d7d4c9513f673dbf069
Neomaster <the@neomaster.net>
2023-02-25 16:20:55 +0300

    [Migration] Audit has been added.

--- Client/src/app/consts/app-version.ts
+++ Client/src/app/consts/app-version.ts
@@ -1,5 +1,5 @@
 ﻿// auto-generated
 export const version = {
   versionNumber: '1.0.0.0',
-  versionDatetime: '2023.02.25 16:12:19',
+  versionDatetime: '2023.02.25 16:19:47',
 };
---
+++ WebApi/Migrations/20230225132015_Audit.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Migrations
+{
+    public partial class Audit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "ngn");
+
+            migrationBuilder.RenameTable(
+                name: "AuditEvent",
+                schema: "mr.apelsin",
+                newName: "AuditEvent",
+                newSchema: "ngn");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "mr.apelsin");
+
+            migrationBuilder.RenameTable(
+                name: "AuditEvent",
+                schema: "ngn",
+                newName: "AuditEvent",
+                newSchema: "mr.apelsin");
+        }
+    }
+}
