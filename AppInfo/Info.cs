bfb2a65d65f40f8a8e6fb3c6e0bd9786df85ee47
Neomaster <the@neomaster.net>
2023-02-25 16:12:15 +0300

    [Infrastructure] AppInfo project has been added.

---
+++ AppInfo/Info.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using Common.Exstensions;
+using Common.Versioning;
+
+namespace AppInfo;
+
+public class Info
+{
+    private static readonly Info _instance = new();
+    public readonly AppVersion AppVersion;
+
+    private Info() => AppVersion = Assembly.GetExecutingAssembly().GetAppVersion();
+    public static Info Instance => _instance;
+}
